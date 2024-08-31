58ce451263f0c12858e9faa6aee9c8cee0931861
mahender762 <mahimahender762@gmail.com>
2024-08-31 19:22:25 +0530

    first commit

---
+++ Key.java
@@ -0,0 +1,14 @@
+public class Key1
+{
+  int x=5;
+
+}
+ class Test
+{
+  public static void main(String args[])
+  {
+     Key k=new Key();
+              
+       System.out.println(k.x);
+      }
+}
